9e5b0a9c43f9949ca594b7a2961ee7385b479471
Caroline Eubanks <cmeubanks@gmail.com>
2021-01-03 16:44:40 -0700

    first commit

--- example1/script.js
+++ example1/script.js
@@ -18,3 +18,22 @@ function createButton() {
   newButton.innerText = "New click me button!";
   parent.appendChild(newButton);
 }
+//addNewButton = selecting "Add a new button below" and
+//firing createButton on a click.
+
+//parent = is assigned to the class of 'parent' in HTML
+//which is our div that contains all 'click-me' Buttons
+
+//allButtons = query selector all grabs all the 'click-me' Buttons
+
+//There is a loop created to iterate through all the 'click me' Buttons
+//and adds an event listener which fires on a 'click' and launches the showAlert
+//function
+
+//createButton function creates a variable newButton which is
+//assigned to the new html element created 'button'
+//.className is a new built in JS method for HTML, which takes our new variable
+//and assigns it to the string of 'click-me' - therefore creating an assigned class
+//.innerText allows us to assign new HTML text to our newButton variable, therefore
+//giving our button a name
+//our 'clickme' buttons are modified with .appendChild
